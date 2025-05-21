d8c25b116087afad9e62a03ebd4d62c25bdd2b89
Amer15 <amer95521@gmail.com>
2025-05-21 11:19:37 +0530

    add: table UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { TodoCard } from "./components/todo-card";
 import { Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -30,6 +38,8 @@ const App = () => {
         const filteredTodos = todos.filter((todo) =>
           todo.title.includes(searchQuery)
         );
+        console.log("executed...");
+        console.log(filteredTodos);
         setFilteredTodos(filteredTodos);
       } else {
         setFilteredTodos([]);
@@ -56,23 +66,54 @@ const App = () => {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      {filteredTodos.length > 0
-        ? filteredTodos.map(({ id, title, userId, completed }) => (
-            <TodoCard
-              key={id}
-              title={title}
-              isCompleted={completed}
-              userId={userId}
-            />
-          ))
-        : todos.map(({ id, title, userId, completed }) => (
-            <TodoCard
-              key={id}
-              title={title}
-              isCompleted={completed}
-              userId={userId}
-            />
-          ))}
+      <Table className="mx-auto max-w-[70%] table-fixed">
+        <TableCaption>A list of todos</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[15%]">User Id</TableHead>
+            <TableHead className="w-[70%] min-w-[200px]">Title</TableHead>
+            <TableHead className="w-[15%]">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {searchQuery &&
+            filteredTodos.length > 0 &&
+            filteredTodos.map(({ id, userId, title, completed }) => (
+              <TableRow key={id}>
+                <TableCell>{userId}</TableCell>
+                <TableCell>{title}</TableCell>
+                <TableCell
+                  className={`${
+                    completed ? "text-green-600" : "text-yellow-600"
+                  }`}
+                >
+                  {completed ? "completed" : "pending"}
+                </TableCell>
+              </TableRow>
+            ))}
+          {searchQuery && filteredTodos.length === 0 && (
+            <TableRow>
+              <TableCell colSpan={3} className="text-center text-gray-500">
+                no results found
+              </TableCell>
+            </TableRow>
+          )}
+          {searchQuery == "" &&
+            todos.map(({ id, userId, title, completed }) => (
+              <TableRow key={id}>
+                <TableCell>{userId}</TableCell>
+                <TableCell>{title}</TableCell>
+                <TableCell
+                  className={`${
+                    completed ? "text-green-600" : "text-yellow-600"
+                  }`}
+                >
+                  {completed ? "completed" : "pending"}
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
     </section>
   );
 };
